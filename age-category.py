fbcde60f017686743bb0b84ecd4a57ac24552e69
saloumeh24 <slmhgh88@gmail.com>
2025-03-22 12:53:36 +0330

    Create age-category.py
    add age category program

---
+++ age-category.py
@@ -0,0 +1,9 @@
+print("***age category***")
+age = int(input("how old are you?"))
+
+if age < 5:
+    print("you are a child!")
+elif age < 13:
+    print("you are a teenager!")
+else:
+    print("you are an adult!")
